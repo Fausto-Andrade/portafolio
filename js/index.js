056510c1ca7b9dd73dd931d2aecd22838eedca84
Fausto Andrade <faustoantonioandradevera@gmail.com>
2024-01-12 12:03:22 -0500

    Mejorando el modulo inicial y el de contacto con animaciones y realizando los enlaces de las redes en todos los modulos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const cursor = document.querySelector(".cursor");
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.navbar-links');
+const links = document.querySelectorAll('.list-link')
 document.addEventListener("mousemove", (e) => {
     cursor.setAttribute('style', 'top: '+e.pageY+'px; let: '+e.pageX+'px;')
     let x = e.pageX;
@@ -13,4 +16,13 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
+})
+
+navbar.addEventListener('click', function (e) {
+  if (e.target.closest('.list-link')){
+    for (const link of links){
+      link.parentElement.classList.remove('active')
+    }
+    e.target.parentElement.classList.add('active')
+  }
 })
